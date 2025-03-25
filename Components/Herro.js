25e679acc75d48a046b1ed007c64af99d3c0e5ab
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2025-03-25 13:44:48 +0300

    adding admision page
    adding admision page

--- Components/Herro.js
+++ Components/Herro.js
@@ -10,6 +10,14 @@ export default function Hero() {
   const slides = [
     { id: 1, imageSrc: "/job2.png" },
     { id: 2, imageSrc: "/job2.png" },
+    { id: 3, imageSrc: "/job2.png" },
+    { id: 4, imageSrc: "/job2.png" },
+    { id: 5, imageSrc: "/job3.png" },
+    { id: 6, imageSrc: "/job2.png" },
+    { id: 7, imageSrc: "/job2.png" },
+    { id: 8, imageSrc: "/job2.png" },
+    { id: 9, imageSrc: "/job2.png" },
+    
   ];
 
   return (
